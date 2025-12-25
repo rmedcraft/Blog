45190800617b7b74bd184db68bfe4eabd8b09672
rmedcraft <rowanmedcraft04@gmail.com>
2025-12-25 17:47:42 -0500

    shadcn set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+
 export default function App() {
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline text-blue-400 w-screen text-center">
+        <div >
+            <h1 className="text-3xlxl underline text-white w-screen text-center">
                 Hello world!
             </h1>
+            <div className="container mx-auto p-8">
+                <Card className="p-6">
+                    <h1 className="text-2xl font-bold mb-4">Vite + shadcn/ui</h1>
+                    <Button>Get started</Button>
+                </Card>
+            </div>
         </div>
     )
 }
