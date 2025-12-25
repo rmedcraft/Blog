9b886574252d98eaba6be53dda810bc8fcfe2fea
rmedcraft <rowanmedcraft04@gmail.com>
2025-12-25 16:54:26 -0500

    tailwind vite react typescript setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline text-blue-400 w-screen text-center">
+                Hello world!
+            </h1>
+        </div>
+    )
+}
